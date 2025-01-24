be7a3a7f8fd7a5cc96cdb0420eea5ca84d4282b7
HabibaAbdelaziz <74095966+HabibaAbdelaziz@users.noreply.github.com>
2025-01-24 17:14:50 +0900

    Set up routing and navigation with protected routes in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,37 @@
+import React from 'react';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Register from './components/Register';
 import MeasurementForm from './components/MeasurementForm';
 import MeasurementHistory from './components/MeasurementHistory';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
 
-const App = () => (
+const App: React.FC = () => {
+  return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/measurements" element={
-              <PrivateRoute>
-                <MeasurementForm />
-              </PrivateRoute>
-            } />
-            <Route path="/measurements/history" element={
-              <PrivateRoute>
-                <MeasurementHistory />
-              </PrivateRoute>
-            } />
-            <Route path="/" element={<Navigate to="/measurements" />} />
-        </Routes>
+      <div className="min-h-screen bg-gray-100">
+        <Navigation />
+        <div className="container mx-auto px-4 py-8">
+          <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/measurements" element={
+                <PrivateRoute>
+                  <MeasurementForm />
+                </PrivateRoute>
+              } />
+              <Route path="/measurements/history" element={
+                <PrivateRoute>
+                  <MeasurementHistory />
+                </PrivateRoute>
+              } />
+              <Route path="/" element={<Navigate to="/measurements" />} />
+          </Routes>
+        </div>
+      </div> 
     </BrowserRouter>
-)
+  );
+};
+
+export default App;
